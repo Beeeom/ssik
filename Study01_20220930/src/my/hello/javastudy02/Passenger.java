1653be727f1438016624693d66005cc3f47b836d
ㅂㅓㅁ ㅅㅣㄱ <108910193+Beeeom@users.noreply.github.com>
2022-09-30 16:49:06 +0900

    Add files via upload

---
+++ Study01_20220930/src/my/hello/javastudy02/Passenger.java
@@ -0,0 +1,27 @@
+package my.hello.javastudy02;
+
+public class Passenger {
+
+	private String name;
+	private String country;
+	private int age;
+	private int id;
+	
+	public Passenger() {
+		
+	}
+	public Passenger
+	(String name) {
+		this.name =name;
+		
+	}
+	public Passenger
+	(String name, String contry, int age, int id) {
+		this.name = name;
+		this.country = country;
+		this.age = age;
+		this.id = id;
+	}
+	
+
+}
