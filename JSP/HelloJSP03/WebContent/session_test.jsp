7215e44391ae53f0a8c675a30e9e12b152928e29
ㅂㅓㅁ ㅅㅣㄱ <108910193+Beeeom@users.noreply.github.com>
2022-10-17 16:41:34 +0900

    JSP3차

---
+++ JSP/HelloJSP03/WebContent/session_test.jsp
@@ -0,0 +1,23 @@
+<%@page import="java.util.Enumeration"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%Enumeration enu = session.getAttributeNames();
+int i = 0;
+while(enu.hasMoreElements()){
+	i++;
+	String sName = enu.nextElement().toString();
+	String sValue = (String) session.getAttribute(sName);
+	System.out.print("sName:"+sName);
+	System.out.print("sValue:"+sValue);
+}
+System.out.println("남아 있는 세션의 수 :"+i);
+%>
+</body>
+</html>
---
+++ JSP/HelloJSP03/src/a/RequestResponse.java
@@ -0,0 +1,85 @@
+package a;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestResponse
+ */
+@WebServlet("/RequestResponse")
+public class RequestResponse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RequestResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+
+	protected void doAction(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// response.setContentType("text/html;charset=UTF-8");
+
+		// request.setCharacterEncoding("UTF-8");
+		// response.setCharacterEncoding("UTF-8");
+		String str = request.getParameter("birth");
+		try {
+			int year = Integer.parseInt(str);
+			Date now = new Date(); // 吏�湲� �떆媛� 媛��졇�샂
+
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy");
+			// �삱�빐瑜� �뒪�듃留곸쑝濡� 諛붽씔 寃� 2022 �씪�뒗 臾몄옄�뿴�씠 �굹�삩�떎.
+			String NowYear = formatter.format(now);
+			int age = Integer.parseInt(NowYear) - year + 1;
+
+			if (age >= 20)
+				response.sendRedirect("pass.jsp?age=" + age);
+			else
+				response.sendRedirect("ng.jsp?age=" + age);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(str + "@@@");
+			// System.out.println(str);
+			// response.encodeRedirectUrl("fail.jsp?birth="+str);
+			response.encodeURL("fail.jsp?birth=" + str);
+			response.setCharacterEncoding("UTF-8");
+			response.sendRedirect("fail.jsp?birth=" + str);
+
+		}
+
+	}
+
+}
